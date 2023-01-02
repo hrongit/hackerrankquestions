c337612c5d560ccc4594b9ed17ff66508fcde49d
HRITIK RAJ <97436607+hrongit@users.noreply.github.com>
2023-01-02 23:30:09 +0530

    hackerrank

---
+++ between_two_sets.py
@@ -0,0 +1,10 @@
+#question - https://www.hackerrank.com/challenges/between-two-sets/problem?isFullScreen=true
+
+input();
+a=list(map(int, input().split()))
+b=list(map(int, input().split()))
+ans=0
+for i in range(1, 101):
+    if all(i%x==0 for x in a) and all(x%i==0 for x in b):
+        ans+=1
+print(ans)
